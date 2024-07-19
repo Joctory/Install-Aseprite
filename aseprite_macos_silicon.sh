501e306a5eaa9c4b864b90a0c07a5d0aadefd75b
Joel Lee Min Kang <61981192+Joctory@users.noreply.github.com>
2024-07-20 01:05:09 +0800

    Update aseprite_macos_silicon.sh

--- aseprite_macos_silicon.sh
+++ aseprite_macos_silicon.sh
@@ -43,3 +43,17 @@ cmake \
 
 # Start aseprite
 $HOME/aseprite/build/bin/aseprite
+
+# Merge the data into Aseprite trial
+mkdir bundle
+cd bundle
+curl -O -J "https://www.aseprite.org/downloads/trial/Aseprite-v1.3.7-trial-macOS.dmg"
+mkdir mount
+yes qy | hdiutil attach -quiet -nobrowse -noverify -noautoopen -mountpoint mount Aseprite-v1.3.4-trial-macOS.dmg
+cp -rf mount/Aseprite.app .
+hdiutil detach mount
+rm -rf Aseprite.app/Contents/MacOS/aseprite
+cp -rf ../aseprite/build/bin/aseprite Aseprite.app/Contents/MacOS/aseprite
+rm -rf Aseprite.app/Contents/Resources/data
+cp -rf ../aseprite/build/bin/data Aseprite.app/Contents/Resources/data
+cd ..
